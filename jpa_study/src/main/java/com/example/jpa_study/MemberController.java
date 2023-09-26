c20942a61d7c90602c3971ae1ad46537a45541a6
Jeongsuwon <129022812+Jeongsuwon@users.noreply.github.com>
2023-09-26 23:28:40 +0900

    oracle db 연동 구성

--- jpa_study/src/main/java/com/example/jpa_study/MemberController.java
+++ jpa_study/src/main/java/com/example/jpa_study/MemberController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MemberController {
 	
-	@GetMapping("test")
+	@GetMapping("/login")
 	public String test(Model model) {
 		
 		model.addAttribute("data", "babo");
